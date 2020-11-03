b44af186c6854d0b782c0a2fe4344f2d9f9fd310
martinbelcic <martin.belcic@gmail.com>
2020-11-03 16:32:00 -0300

    Menu SEL

--- SEL/main.f90
+++ SEL/main.f90
@@ -4,7 +4,7 @@ program principal
 
     implicit none
 
-    integer, parameter :: orden = 4
+    integer, parameter :: orden = 4 !Modificar
     real(8) matriz(orden, orden), term_ind(orden, 1), xini(orden, 1), matriz2(3, 3)
 
     xini = 0
@@ -64,6 +64,61 @@ program principal
     write(*, *)
     write(*, *) "Chequeo matriz inversa:"
     call mostrarMatriz(matmul(matriz2, matrizInversa(matriz2)))
+
 contains
+
+    subroutine Menu(matriz, term_ind, xini)
+        real(8) matriz(orden, orden), term_ind(orden, 1), xini(orden, 1)
+        logical :: op
+        integer select
+
+        op = .true.
+
+        do while(op)
+            write(*, *) "Seleccione una Opcion:"
+            write(*, *) "1 - Gauss"
+            write(*, *) "2 - Gauss con Refinamiento Iterativo"
+            write(*, *) "3 - Jacobi"
+            write(*, *) "4 - Gauss Seidel"
+            write(*, *) "5 - Matriz inversa"
+            write(*, *) "0 - Salir"
+
+            read (*, *) select
+
+            select case (select)
+                case (1)
+                    write(*,*) "----"
+                    write(*,*) "Gaus"
+                    write(*,*) "----"
+                    call mostrarMatriz(solucionGauss(matriz, term_ind))
+                case (2)
+                    write(*,*) "-------------------------------"
+                    write(*,*) "Gaus con Refinamiento Iterativo"
+                    write(*,*) "-------------------------------"
+                    call mostrarMatriz(refinamientoIter(matriz, term_ind, 0.000000000001_8, solucionGauss, mNormaM))
+                case (3)
+                    write(*,*) "------"
+                    write(*,*) "Jacobi"
+                    write(*,*) "------"
+                    call mostrarMatriz(jacobi(matriz, term_ind, xini, 0.000000000001_8))
+                case (4)
+                    write(*,*) "------------"
+                    write(*,*) "Gauss Seidel"
+                    write(*,*) "------------"
+                    call mostrarMatriz(gaussSeidel(matriz, term_ind, xini, 0.000000000001_8))
+                case (5)
+                    write(*, *) "--------------"
+                    write(*, *) "Matriz Inversa"
+                    write(*, *) "--------------"
+                    call mostrarMatriz(matrizInversa(matriz))
+                case (0)
+                    write(*, *) "Saliendo"
+                    op = .false.
+            end select
+
+        end do
+
+    end subroutine Menu
+
 
 end program principal
