6ffeeebe225e7bd012e8662ad983c41b27c6a78b
martinbelcic <martin.belcic@gmail.com>
2020-11-10 11:41:17 -0300

    Menu SEL casi completo

--- SEL/main.f90
+++ SEL/main.f90
@@ -4,10 +4,10 @@ program principal
 
     implicit none
 
-    integer, parameter :: orden = 4 !Modificar
+    integer, parameter :: orden = 6 !Modificar
     real(8) matriz(orden, orden), term_ind(orden, 1), xini(orden, 1), matriz2(3, 3)
 
-    xini = 0
+    xini = 0 !PUEDE LLEGAR A MODIFICARSE
     call leerMatriz(matriz, "matriz_ejemplo1.txt")
     call leerMatriz(term_ind, "independientes_ejemplo1.txt")
 
@@ -21,12 +21,6 @@ program principal
     write(*, *) "Norma F ", mNormaF(matriz)
     write(*, *)
 
-    call pivotear(matriz, term_ind)
-    call mostrarMatriz(matriz)
-    write(*, *)
-    call mostrarMatriz(term_ind)
-    write(*, *)
-    read(*, *)
     call Menu(matriz, term_ind, xini)
 
 !    write(*, *) "Solucion Gauss:"
@@ -55,7 +49,7 @@ program principal
 contains
 
     subroutine Menu(matriz, term_ind, xini)
-        real(8) matriz(orden, orden), term_ind(orden, 1), xini(orden, 1)
+        real(8) matriz(orden, orden), term_ind(orden, 1), xini(orden, 1), pert(orden, 1), mat_pert(orden, orden)
         logical :: op
         integer select
 
@@ -69,6 +63,10 @@ contains
             write(*, *) "4 - Gauss Seidel"
             write(*, *) "5 - Matriz inversa"
             write(*, *) "6 - Numero de Condicion"
+            write(*, *) "7 - Gaus Jordan"
+            write(*, *) "8 - Pivotear"
+            write(*, *) "9 - Error Relativo Vector"
+            write(*, *) "10 - Error Relativo Matriz"
             write(*, *) "0 - Salir"
 
             read (*, *) select
@@ -88,7 +86,7 @@ contains
                     write(*,*) "------"
                     write(*,*) "Jacobi"
                     write(*,*) "------"
-                    call mostrarMatriz(jacobi(matriz, term_ind, xini, 0.000000000001_8))
+                    call mostrarMatriz(jacobi(matriz, term_ind, xini, 0.0000005_8))
                 case (4)
                     write(*,*) "------------"
                     write(*,*) "Gauss Seidel"
@@ -103,7 +101,34 @@ contains
                     write(*, *) "-------------------"
                     write(*, *) "Numero de condicion"
                     write(*, *) "-------------------"
-                    write(*, "(f20.15)") condicion(matriz, mNormaM) !MODIFICAR NORMA SI HACE FALTA
+                    write(*, "(f20.10)") condicion(matriz, mNormaM) !MODIFICAR NORMA SI HACE FALTA
+                case (7)
+                    write(*, *) "------------"
+                    write(*, *) "Gauss Jordan"
+                    write(*, *) "------------"
+                    call mostrarMatriz(solucionGaussJordan(matriz, term_ind))
+                case (8)
+                    write(*, *) "--------"
+                    write(*, *) "Pivotear"
+                    write(*, *) "--------"
+                    call pivotear(matriz, term_ind)
+                    call mostrarMatriz(matriz)
+                    write(*, *)
+                    call mostrarMatriz(term_ind)
+                    write(*, *)
+                case (9)
+                    write(*, *) "---------------------"
+                    write(*, *) "Error Relativo Vector"
+                    write(*, *) "---------------------"
+                    call leerMatriz(pert, "perturbacion_ti.txt")
+                    write(*, "(f20.10)") errorRelativo(term_ind, pert, mNormaM) !MODIFICAR NORMA SI HACE FALTA
+
+                case (10)
+                    write(*, *) "---------------------"
+                    write(*, *) "Error Relativo Matriz"
+                    write(*, *) "---------------------"
+                    call leerMatriz(mat_pert, "perturbacion_matriz.txt")
+                    write(*, "(f20.10)") errorRelativo(matriz, mat_pert, mNormaM) !MODIFICAR NORMA SI HACE FALTA
                 case (0)
                     write(*, *) "Saliendo..."
                     op = .false.
